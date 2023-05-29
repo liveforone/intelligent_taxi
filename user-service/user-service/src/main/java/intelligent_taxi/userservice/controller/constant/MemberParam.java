6ac0b21d060526b69abcc135c70dd2c07602bdec
liveforone <yc4852@gmail.com>
2023-05-29 22:34:44 +0900

    Add member param.

---
+++ user-service/user-service/src/main/java/intelligent_taxi/userservice/controller/constant/MemberParam.java
@@ -0,0 +1,7 @@
+package intelligent_taxi.userservice.controller.constant;
+
+public final class MemberParam {
+    private MemberParam() {}
+
+    public static final String USERNAME = "username";
+}
