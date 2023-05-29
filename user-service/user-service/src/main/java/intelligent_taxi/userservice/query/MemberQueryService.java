072c1ceb0517598ebb95704ffbd5a962398aa2de
liveforone <yc4852@gmail.com>
2023-05-29 22:33:23 +0900

    Add query service.

---
+++ user-service/user-service/src/main/java/intelligent_taxi/userservice/query/MemberQueryService.java
@@ -0,0 +1,26 @@
+package intelligent_taxi.userservice.query;
+
+import intelligent_taxi.userservice.dto.response.MemberResponse;
+import intelligent_taxi.userservice.query.util.MemberMapper;
+import intelligent_taxi.userservice.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MemberQueryService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberResponse getMemberByUsername(String username) {
+        return MemberMapper.entityToDto(memberRepository.findByUsername(username));
+    }
+
+    public String getBankbookNumByUsername(String username) {
+        return memberRepository.findBankbookNumByUsername(username);
+    }
+}
