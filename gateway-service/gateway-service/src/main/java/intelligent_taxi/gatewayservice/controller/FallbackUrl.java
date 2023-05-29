4bc4d533ca14da05768c34e74035ed76da95887f
liveforone <yc4852@gmail.com>
2023-05-29 14:01:48 +0900

    Add fallback logic.

---
+++ gateway-service/gateway-service/src/main/java/intelligent_taxi/gatewayservice/controller/FallbackUrl.java
@@ -0,0 +1,7 @@
+package intelligent_taxi.gatewayservice.controller;
+
+public final class FallbackUrl {
+
+    public static final String BASE = "/fallback";
+    public static final String USER = "/user";
+}
