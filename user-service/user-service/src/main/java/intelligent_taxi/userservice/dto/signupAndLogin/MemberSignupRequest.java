efe38975a12e3d8c6d5f959f875127d159f0f9d8
liveforone <yc4852@gmail.com>
2023-05-29 22:27:47 +0900

    Add member request dtos.

---
+++ user-service/user-service/src/main/java/intelligent_taxi/userservice/dto/signupAndLogin/MemberSignupRequest.java
@@ -0,0 +1,24 @@
+package intelligent_taxi.userservice.dto.signupAndLogin;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class MemberSignupRequest {
+
+    @Email(message = "이메일 형식을 지켜 작성해주세요.")
+    @NotBlank(message = "이메일은 반드시 기입해주세요.")
+    private String email;
+
+    @NotBlank(message = "비밀번호를 반드시 입력하세요.")
+    private String password;
+
+    @NotBlank(message = "실명은 반드시 기입해야하며, 잘못된 실명기입은 소비자가 책임집니다.")
+    private String realName;
+
+    @NotBlank(message = "Intelligent Pay 계좌 번호를 입력하세요.")
+    private String bankbookNum;
+}
