2ac955215b00be24627c8fc1cbc18d26c410874c
liveforone <yc4852@gmail.com>
2023-05-29 22:32:01 +0900

    Add member constant.

---
+++ user-service/user-service/src/main/java/intelligent_taxi/userservice/domain/util/MemberConstant.java
@@ -0,0 +1,7 @@
+package intelligent_taxi.userservice.domain.util;
+
+public final class MemberConstant {
+
+    public static final int RANDOM_STRING_LENGTH = 4;
+    public static final int ONE = 1;
+}
