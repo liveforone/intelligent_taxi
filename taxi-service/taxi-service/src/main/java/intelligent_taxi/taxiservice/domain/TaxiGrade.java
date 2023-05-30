8eab78c52e93a7efa726ec3f3e5a40386593ab98
liveforone <yc4852@gmail.com>
2023-05-30 19:18:19 +0900

    Add match grade func.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/domain/TaxiGrade.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/domain/TaxiGrade.java
@@ -1,5 +1,17 @@
 package intelligent_taxi.taxiservice.domain;
 
+import intelligent_taxi.taxiservice.controller.restResponse.ResponseMessage;
+import intelligent_taxi.taxiservice.exception.TaxiCustomException;
+
 public enum TaxiGrade {
-    NORMAL, PREMIUM, VAN
+    NORMAL, PREMIUM, VAN;
+
+    public static TaxiGrade matchGrade(String grade) {
+        for (TaxiGrade taxiGrade : TaxiGrade.values()) {
+            if (taxiGrade.name().equalsIgnoreCase(grade)) {
+                return taxiGrade;
+            }
+        }
+        throw new TaxiCustomException(ResponseMessage.NOT_EXIST_GRADE);
+    }
 }
