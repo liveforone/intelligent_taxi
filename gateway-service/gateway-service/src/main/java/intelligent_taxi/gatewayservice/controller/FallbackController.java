d1898f1967b13f4dde18c9c153277418fe773ea0
liveforone <yc4852@gmail.com>
2023-05-30 16:49:31 +0900

    Add taxi-service.

--- gateway-service/gateway-service/src/main/java/intelligent_taxi/gatewayservice/controller/FallbackController.java
+++ gateway-service/gateway-service/src/main/java/intelligent_taxi/gatewayservice/controller/FallbackController.java
@@ -18,12 +18,27 @@ public class FallbackController {
     }
 
     @PutMapping(FallbackUrl.USER)
-    public Mono<String> fallbackUserPatch() {
+    public Mono<String> fallbackUserPut() {
         return Mono.just(FallbackMessage.USER_LOG);
     }
 
     @DeleteMapping(FallbackUrl.USER)
     public Mono<String> fallbackUserDelete() {
         return Mono.just(FallbackMessage.USER_LOG);
+    }
+
+    @GetMapping(FallbackUrl.TAXI)
+    public Mono<String> fallbackTaxiGet() {
+        return Mono.just(FallbackMessage.TAXI_LOG);
+    }
+
+    @PostMapping(FallbackUrl.TAXI)
+    public Mono<String> fallbackTaxiPost() {
+        return Mono.just(FallbackMessage.TAXI_LOG);
+    }
+
+    @PutMapping(FallbackUrl.TAXI)
+    public Mono<String> fallbackTaxiPut() {
+        return Mono.just(FallbackMessage.TAXI_LOG);
     }
 }
--- gateway-service/gateway-service/src/main/java/intelligent_taxi/gatewayservice/controller/FallbackMessage.java
+++ gateway-service/gateway-service/src/main/java/intelligent_taxi/gatewayservice/controller/FallbackMessage.java
@@ -2,4 +2,5 @@ package intelligent_taxi.gatewayservice.controller;
 
 public final class FallbackMessage {
     public static final String USER_LOG = "유저 서비스의 장애로 접근이 불가능합니다.\n이용에 불편을 드려 죄송합니다.";
+    public static final String TAXI_LOG = "택시 서비스의 장애로 접근이 불가능합니다.\n이용에 불편을 드려 죄송합니다.";
 }
