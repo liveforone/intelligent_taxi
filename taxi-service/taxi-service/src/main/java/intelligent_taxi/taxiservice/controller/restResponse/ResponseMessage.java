6c2740063441427f6d171f1223ec3a6ce8113ff1
liveforone <yc4852@gmail.com>
2023-05-30 19:16:59 +0900

    Add not exist region message.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/restResponse/ResponseMessage.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/restResponse/ResponseMessage.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseMessage {
 
+    NOT_EXIST_REGION(404, "존재하지 않는 지역입니다."),
     SAVE_TAXI_SUCCESS(201, "택시 등록에 성공하였습니다.");
 
     private final int status;
