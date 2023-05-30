dce928b8d61ebc81d12990a4d2cd36ba9a89e5f6
liveforone <yc4852@gmail.com>
2023-05-30 18:54:16 +0900

    Refactor taxi grade field.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/dto/TaxiRequest.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/dto/TaxiRequest.java
@@ -12,7 +12,9 @@ public class TaxiRequest {
 
     @NotBlank(message = "지역을 선택하세요.")
     private String region;
-    private TaxiGrade taxiGrade;
+
+    @NotBlank(message = "택시 등급을 선택하세요.")
+    private String taxiGrade;
 
     @NotBlank(message = "차량 번호를 입력하세요.(번호판 참조)")
     private String licenseNum;
