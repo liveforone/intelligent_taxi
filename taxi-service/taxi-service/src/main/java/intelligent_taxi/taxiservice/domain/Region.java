0346f108c8e798e9af333148b12e63186f487bcf
liveforone <yc4852@gmail.com>
2023-05-30 18:54:24 +0900

    Add value in region.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/domain/Region.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/domain/Region.java
@@ -1,17 +1,24 @@
 package intelligent_taxi.taxiservice.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Region {
 
     //==수도권==//
-    SEOUL, GYEONGGI, INCHEON,
+    SEOUL("서울"), GYEONGGI("경기"), INCHEON("인천"),
     //==강원==//
-    GANGWON,
+    GANGWON("강원"),
     //==충청==//
-    CHUNGBUK, CHUNGNAM, DAEJEON,
+    CHUNGBUK("충북"), CHUNGNAM("충남"), DAEJEON("대전"),
     //==경상==//
-    GYEONGBUK, GYEONGNAM, DAEGU, ULSAN, BUSAN,
+    GYEONGBUK("경북"), GYEONGNAM("경남"), DAEGU("대구"), ULSAN("울산"), BUSAN("부산"),
     //==전라==//
-    JEOLLABUK, JEOLLANAM, GWANGJU,
+    JEOLLABUK("전북"), JEOLLANAM("전남"), GWANGJU("광주"),
     //==제주==//
-    JEJU;
+    JEJU("제주");
+    
+    private final String region;
 }
