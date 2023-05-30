a057395734c4509dd9221cd5f1a07c9a5f7b7714
liveforone <yc4852@gmail.com>
2023-05-30 20:51:00 +0900

    Refactor auth and member state type.

--- user-service/user-service/src/main/java/intelligent_taxi/userservice/dto/response/MemberResponse.java
+++ user-service/user-service/src/main/java/intelligent_taxi/userservice/dto/response/MemberResponse.java
@@ -1,7 +1,5 @@
 package intelligent_taxi.userservice.dto.response;
 
-import intelligent_taxi.userservice.domain.MemberState;
-import intelligent_taxi.userservice.domain.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,6 +14,6 @@ public class MemberResponse {
     private Long id;
     private String email;
     private String realName;
-    private Role auth;
-    private MemberState memberState;
+    private String auth;
+    private String memberState;
 }
