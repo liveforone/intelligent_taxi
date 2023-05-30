7207ae195f538640a972ac70357ccb6b61e87e48
liveforone <yc4852@gmail.com>
2023-05-30 19:48:24 +0900

    Add taxi domain test.

---
+++ taxi-service/taxi-service/src/test/java/intelligent_taxi/taxiservice/domain/TaxiTest.java
@@ -0,0 +1,77 @@
+package intelligent_taxi.taxiservice.domain;
+
+import intelligent_taxi.taxiservice.dto.TaxiRequest;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class TaxiTest {
+
+    @Test
+    void createTest() {
+        //given
+        String username = "jdfoajeofjaoifeoincojeofjaoefjofja";
+        String licenseNum = "00아 0000";
+        String phoneNum = "01000000000";
+        String region = "서울";
+        String grade = "van";
+        TaxiRequest request = new TaxiRequest();
+        request.setLicenseNum(licenseNum);
+        request.setRegion(region);
+        request.setPhoneNum(phoneNum);
+        request.setTaxiGrade(grade);
+
+        //when
+        Taxi taxi = Taxi.create(request, username);
+
+        //then
+        assertThat(taxi.getTaxiGrade()).isEqualTo(TaxiGrade.VAN);
+        assertThat(taxi.getRegion()).isEqualTo(Region.SEOUL);
+    }
+
+    @Test
+    void updateRegionTest() {
+        //given
+        String username = "jdfoajeofjaoifeoincojeofjaoefjofja";
+        String licenseNum = "00아 0000";
+        String phoneNum = "01000000000";
+        String region = "서울";
+        String grade = "PREMIUM";
+        TaxiRequest request = new TaxiRequest();
+        request.setLicenseNum(licenseNum);
+        request.setRegion(region);
+        request.setPhoneNum(phoneNum);
+        request.setTaxiGrade(grade);
+        Taxi taxi = Taxi.create(request, username);
+
+        //when
+        String updatedRegion = "인천";
+        taxi.updateRegion(updatedRegion);
+
+        //then
+        assertThat(taxi.getRegion()).isEqualTo(Region.INCHEON);
+    }
+
+    @Test
+    void updateGradeTest() {
+        //given
+        String username = "jdfoajeofjaoifeoincojeofjaoefjofja";
+        String licenseNum = "00아 0000";
+        String phoneNum = "01000000000";
+        String region = "서울";
+        String grade = "PREMIUM";
+        TaxiRequest request = new TaxiRequest();
+        request.setLicenseNum(licenseNum);
+        request.setRegion(region);
+        request.setPhoneNum(phoneNum);
+        request.setTaxiGrade(grade);
+        Taxi taxi = Taxi.create(request, username);
+
+        //when
+        String updatedGrade = "normal";
+        taxi.updateGrade(updatedGrade);
+
+        //then
+        assertThat(taxi.getTaxiGrade()).isEqualTo(TaxiGrade.NORMAL);
+    }
+}
