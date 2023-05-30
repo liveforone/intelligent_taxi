07c9a849faba64607895cea397e92c89fe93d9ca
liveforone <yc4852@gmail.com>
2023-05-30 18:32:33 +0900

    Add region class.

---
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/domain/Region.java
@@ -0,0 +1,17 @@
+package intelligent_taxi.taxiservice.domain;
+
+public enum Region {
+
+    //==수도권==//
+    SEOUL, GYEONGGI, INCHEON,
+    //==강원==//
+    GANGWON,
+    //==충청==//
+    CHUNGBUK, CHUNGNAM, DAEJEON,
+    //==경상==//
+    GYEONGBUK, GYEONGNAM, DAEGU, ULSAN, BUSAN,
+    //==전라==//
+    JEOLLABUK, JEOLLANAM, GWANGJU,
+    //==제주==//
+    JEJU;
+}
