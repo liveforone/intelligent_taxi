e7a012e53803d15fe8fd1282783a0633042879f9
liveforone <yc4852@gmail.com>
2023-06-01 17:28:00 +0900

    Refactor match grade return type.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/domain/TaxiGrade.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/domain/TaxiGrade.java
@@ -6,10 +6,10 @@ import intelligent_taxi.taxiservice.exception.TaxiCustomException;
 public enum TaxiGrade {
     NORMAL, PREMIUM, VAN;
 
-    public static String matchGrade(String grade) {
+    public static TaxiGrade matchGrade(String grade) {
         for (TaxiGrade taxiGrade : TaxiGrade.values()) {
             if (taxiGrade.name().equalsIgnoreCase(grade)) {
-                return taxiGrade.name();
+                return taxiGrade;
             }
         }
         throw new TaxiCustomException(ResponseMessage.NOT_EXIST_GRADE);
