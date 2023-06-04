be241f1fdfb9c9fb45e9fa5480a44d5a1631939e
liveforone <yc4852@gmail.com>
2023-06-04 21:20:30 +0900

    Add controller log.

---
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/constant/ControllerLog.java
@@ -0,0 +1,13 @@
+package intelligent_taxi.taxiservice.controller.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ControllerLog {
+
+    CREATE_TAXI_SUCCESS("Taxi Create Success!!");
+
+    private final String log;
+}
