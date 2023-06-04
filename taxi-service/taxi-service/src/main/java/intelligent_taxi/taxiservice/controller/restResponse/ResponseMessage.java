5f9121efd919662ea491c1471cf0d6d08b653b5a
liveforone <yc4852@gmail.com>
2023-06-04 22:12:23 +0900

    Add update region response message.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/restResponse/ResponseMessage.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/restResponse/ResponseMessage.java
@@ -12,7 +12,8 @@ public enum ResponseMessage {
     NOT_EXIST_GRADE(404, "존재하지 않는 택시 등급입니다."),
     CREATE_TAXI_SUCCESS(201, "택시 등록에 성공하였습니다."),
     TAXI_IS_NULL(404, "존재하지 않는 택시입니다."),
-    NOT_MATCH_USERNAME(400, "기사 정보가 일치하지 않습니다.");
+    NOT_MATCH_USERNAME(400, "기사 정보가 일치하지 않습니다."),
+    UPDATE_REGION_SUCCESS(200, "지역 변경에 성공하였습니다.");
 
     private final int status;
     private final String value;
