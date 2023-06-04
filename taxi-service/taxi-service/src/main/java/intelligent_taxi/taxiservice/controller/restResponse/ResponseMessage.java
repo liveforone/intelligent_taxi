d57b2a599f2654caaa79837bf80cd7a50d74ec6e
liveforone <yc4852@gmail.com>
2023-06-04 21:49:41 +0900

    Add taxi is null message.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/restResponse/ResponseMessage.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/restResponse/ResponseMessage.java
@@ -10,7 +10,8 @@ public enum ResponseMessage {
     NOT_TAXI_MEMBER(401, "택시 권한의 회원이 아닙니다."),
     NOT_EXIST_REGION(404, "존재하지 않는 지역입니다."),
     NOT_EXIST_GRADE(404, "존재하지 않는 택시 등급입니다."),
-    CREATE_TAXI_SUCCESS(201, "택시 등록에 성공하였습니다.");
+    CREATE_TAXI_SUCCESS(201, "택시 등록에 성공하였습니다."),
+    TAXI_IS_NULL(404, "존재하지 않는 택시입니다.");
 
     private final int status;
     private final String value;
