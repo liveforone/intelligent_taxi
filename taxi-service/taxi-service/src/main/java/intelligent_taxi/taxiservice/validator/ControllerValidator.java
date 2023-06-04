87c384bf44f7ca9179708c33fbf96f368712e08c
liveforone <yc4852@gmail.com>
2023-06-04 21:18:04 +0900

    Add validate binding method.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/validator/ControllerValidator.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/validator/ControllerValidator.java
@@ -2,11 +2,24 @@ package intelligent_taxi.taxiservice.validator;
 
 import intelligent_taxi.taxiservice.authentication.Role;
 import intelligent_taxi.taxiservice.controller.restResponse.ResponseMessage;
+import intelligent_taxi.taxiservice.exception.BindingCustomException;
 import intelligent_taxi.taxiservice.exception.TaxiCustomException;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+import java.util.Objects;
 
 @Component
 public class ControllerValidator {
+
+    public void validateBinding(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorMessage = Objects
+                    .requireNonNull(bindingResult.getFieldError())
+                    .getDefaultMessage();
+            throw new BindingCustomException(errorMessage);
+        }
+    }
 
     public void validateAuth(String auth) {
         if (!auth.equals(Role.TAXI.getAuth())) {
