e271e53fa5799f134e5ccc04faf964020719eff4
liveforone <yc4852@gmail.com>
2023-06-04 21:40:41 +0900

    Add update region url.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/constant/TaxiUrl.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/constant/TaxiUrl.java
@@ -3,4 +3,5 @@ package intelligent_taxi.taxiservice.controller.constant;
 public final class TaxiUrl {
 
     public static final String CREATE = "/create/taxi";
+    public static final String UPDATE_REGION = "/update/region/{id}";
 }
