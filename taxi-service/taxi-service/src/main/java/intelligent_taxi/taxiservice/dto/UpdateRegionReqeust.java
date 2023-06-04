ea6a8b956b5290fc025d7c2d2f8c2ee43183d537
liveforone <yc4852@gmail.com>
2023-06-04 22:17:55 +0900

    Refactor rename update dto.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/dto/UpdateRegionReqeust.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/dto/UpdateRegionReqeust.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class UpdateRegion {
+public class UpdateRegionReqeust {
 
     @NotBlank(message = "지역을 선택하세요.")
     private String region;
