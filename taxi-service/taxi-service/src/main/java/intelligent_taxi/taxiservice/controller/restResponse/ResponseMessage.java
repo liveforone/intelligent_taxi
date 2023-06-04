8410ecc40fb8c4c4f5d35e38e3b6052748d034c1
liveforone <yc4852@gmail.com>
2023-06-04 21:09:17 +0900

    Add not taxi member response message.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/restResponse/ResponseMessage.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/restResponse/ResponseMessage.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseMessage {
 
+    NOT_TAXI_MEMBER(401, "택시 권한의 회원이 아닙니다."),
     NOT_EXIST_REGION(404, "존재하지 않는 지역입니다."),
     NOT_EXIST_GRADE(404, "존재하지 않는 택시 등급입니다."),
     SAVE_TAXI_SUCCESS(201, "택시 등록에 성공하였습니다.");
