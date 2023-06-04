a50f209c504bfebc39118dc426d8deb5dd74c234
liveforone <yc4852@gmail.com>
2023-06-04 21:50:30 +0900

    Add not match username message.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/restResponse/ResponseMessage.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/restResponse/ResponseMessage.java
@@ -11,7 +11,8 @@ public enum ResponseMessage {
     NOT_EXIST_REGION(404, "존재하지 않는 지역입니다."),
     NOT_EXIST_GRADE(404, "존재하지 않는 택시 등급입니다."),
     CREATE_TAXI_SUCCESS(201, "택시 등록에 성공하였습니다."),
-    TAXI_IS_NULL(404, "존재하지 않는 택시입니다.");
+    TAXI_IS_NULL(404, "존재하지 않는 택시입니다."),
+    NOT_MATCH_USERNAME(400, "기사 정보가 일치하지 않습니다.");
 
     private final int status;
     private final String value;
