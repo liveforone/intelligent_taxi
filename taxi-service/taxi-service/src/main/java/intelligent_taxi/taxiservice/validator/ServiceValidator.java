0ed8aa461746cbfeaf00e76eb16ef7b100da8098
liveforone <yc4852@gmail.com>
2023-06-04 22:09:23 +0900

    Add update region validator.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/validator/ServiceValidator.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/validator/ServiceValidator.java
@@ -1,6 +1,10 @@
 package intelligent_taxi.taxiservice.validator;
 
+import intelligent_taxi.taxiservice.controller.restResponse.ResponseMessage;
+import intelligent_taxi.taxiservice.domain.Taxi;
+import intelligent_taxi.taxiservice.exception.TaxiCustomException;
 import intelligent_taxi.taxiservice.repository.TaxiRepository;
+import intelligent_taxi.taxiservice.utility.CommonUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -9,4 +13,16 @@ import org.springframework.stereotype.Component;
 public class ServiceValidator {
 
     private final TaxiRepository taxiRepository;
+
+    public void validateUpdateRegion(String username, Long id) {
+        Taxi taxi = taxiRepository.findOneById(id);
+
+        if (CommonUtils.isNull(taxi)) {
+            throw new TaxiCustomException(ResponseMessage.TAXI_IS_NULL);
+        }
+
+        if (!taxi.getUsername().equals(username)) {
+            throw new TaxiCustomException(ResponseMessage.NOT_MATCH_USERNAME);
+        }
+    }
 }
