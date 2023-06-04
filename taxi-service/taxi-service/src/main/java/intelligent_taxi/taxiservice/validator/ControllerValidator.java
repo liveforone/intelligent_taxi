4f997b05628999482e446face5677bc17ecf789d
liveforone <yc4852@gmail.com>
2023-06-04 21:15:56 +0900

    Add validate auth method.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/validator/ControllerValidator.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/validator/ControllerValidator.java
@@ -1,6 +1,16 @@
 package intelligent_taxi.taxiservice.validator;
 
+import intelligent_taxi.taxiservice.authentication.Role;
+import intelligent_taxi.taxiservice.controller.restResponse.ResponseMessage;
+import intelligent_taxi.taxiservice.exception.TaxiCustomException;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ControllerValidator {
 
-
+    public void validateAuth(String auth) {
+        if (!auth.equals(Role.TAXI.getAuth())) {
+            throw new TaxiCustomException(ResponseMessage.NOT_TAXI_MEMBER);
+        }
+    }
 }
