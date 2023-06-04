bed73fc27a21395ffb046b49347b91ce86ba7130
liveforone <yc4852@gmail.com>
2023-06-04 22:09:33 +0900

    Add update region service method.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/command/TaxiCommandService.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/command/TaxiCommandService.java
@@ -2,7 +2,9 @@ package intelligent_taxi.taxiservice.command;
 
 import intelligent_taxi.taxiservice.domain.Taxi;
 import intelligent_taxi.taxiservice.dto.TaxiRequest;
+import intelligent_taxi.taxiservice.dto.UpdateRegion;
 import intelligent_taxi.taxiservice.repository.TaxiRepository;
+import intelligent_taxi.taxiservice.validator.ServiceValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,9 +15,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class TaxiCommandService {
 
     private final TaxiRepository taxiRepository;
+    private final ServiceValidator serviceValidator;
 
     public Long createTaxi(TaxiRequest requestDto, String username) {
         Taxi taxi = Taxi.create(requestDto, username);
         return taxiRepository.save(taxi).getId();
+    }
+
+    public void updateRegion(UpdateRegion requestDto, String username, Long id) {
+        serviceValidator.validateUpdateRegion(username, id);
+
+        Taxi taxi = taxiRepository.findOneById(id);
+        taxi.updateRegion(requestDto.getRegion());
     }
 }
