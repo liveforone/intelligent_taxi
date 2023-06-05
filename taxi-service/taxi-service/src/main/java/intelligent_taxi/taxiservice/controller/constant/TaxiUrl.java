d4a6be95560d508d12e9cb6f39786a0e20f5cedf
liveforone <yc4852@gmail.com>
2023-06-05 17:32:02 +0900

    Remove update license num and phone num

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/constant/TaxiUrl.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/constant/TaxiUrl.java
@@ -5,6 +5,4 @@ public final class TaxiUrl {
     public static final String CREATE = "/create/taxi";
     public static final String UPDATE_REGION = "/update/region/{id}";
     public static final String UPDATE_GRADE = "/update/grade/{id}";
-    public static final String UPDATE_LICENSE_NUM = "/update/license/{id}";
-    public static final String UPDATE_PHONE_NUM = "/update/phone/{id}";
 }
