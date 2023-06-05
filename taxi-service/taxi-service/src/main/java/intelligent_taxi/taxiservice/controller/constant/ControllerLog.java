5de555cddb9a7110a987350e9744497e75b733a0
liveforone <yc4852@gmail.com>
2023-06-05 17:39:16 +0900

    Add delete taxi success log.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/constant/ControllerLog.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/constant/ControllerLog.java
@@ -9,7 +9,8 @@ public enum ControllerLog {
 
     CREATE_TAXI_SUCCESS("Taxi Create Success!!"),
     UPDATE_REGION_SUCCESS("Update Region Success | ID : "),
-    UPDATE_GRADE_SUCCESS("Update Grade Success | ID : ");
+    UPDATE_GRADE_SUCCESS("Update Grade Success | ID : "),
+    DELETE_TAXI_SUCCESS("Delete Taxi Success | ID : ");
 
     private final String log;
 }
