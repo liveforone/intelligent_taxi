943840dfa2b9eccebfb09a73fb7821f8d1585c71
liveforone <yc4852@gmail.com>
2023-06-05 18:23:26 +0900

    Add update region test.

--- taxi-service/taxi-service/src/test/java/intelligent_taxi/taxiservice/command/TaxiCommandServiceTest.java
+++ taxi-service/taxi-service/src/test/java/intelligent_taxi/taxiservice/command/TaxiCommandServiceTest.java
@@ -1,6 +1,8 @@
 package intelligent_taxi.taxiservice.command;
 
+import intelligent_taxi.taxiservice.domain.Region;
 import intelligent_taxi.taxiservice.dto.TaxiRequest;
+import intelligent_taxi.taxiservice.dto.UpdateRegionRequest;
 import intelligent_taxi.taxiservice.query.TaxiQueryService;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Test;
@@ -21,6 +23,15 @@ class TaxiCommandServiceTest {
 
     @Autowired
     TaxiQueryService taxiQueryService;
+
+    Long createTaxi(String username, String region, String taxiGrade, String licenseNum, String phoneNum) {
+        TaxiRequest request = new TaxiRequest();
+        request.setRegion(region);
+        request.setTaxiGrade(taxiGrade);
+        request.setLicenseNum(licenseNum);
+        request.setPhoneNum(phoneNum);
+        return taxiCommandService.createTaxi(request, username);
+    }
 
     @Test
     @Transactional
@@ -50,7 +61,27 @@ class TaxiCommandServiceTest {
     @Test
     @Transactional
     void updateRegionTest() {
+        //given
+        String username = "dflsjfoefneowfoaefoijf";
+        String region = "seoul";
+        String taxiGrade = "NORMAL";
+        String licenseNum = "00아0000";
+        String phoneNum = "01000000000";
+        Long taxiId = createTaxi(username, region, taxiGrade, licenseNum, phoneNum);
+        em.flush();
+        em.clear();
 
+        //when
+        String updatedRegion = "GYEONGGI";
+        UpdateRegionRequest request = new UpdateRegionRequest();
+        request.setRegion(updatedRegion);
+        taxiCommandService.updateRegion(request, username, taxiId);
+        em.flush();
+        em.clear();
+
+        //then
+        assertThat(taxiQueryService.getTaxiById(taxiId).getRegion())
+                .isEqualTo(Region.GYEONGGI.getRegion());
     }
 
     @Test
