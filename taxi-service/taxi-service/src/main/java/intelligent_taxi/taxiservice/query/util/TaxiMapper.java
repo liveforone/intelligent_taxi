c2417a6725238928f57a661839c5a39e2ddaeb31
liveforone <yc4852@gmail.com>
2023-06-05 17:56:02 +0900

    Add null check.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/query/util/TaxiMapper.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/query/util/TaxiMapper.java
@@ -2,9 +2,14 @@ package intelligent_taxi.taxiservice.query.util;
 
 import intelligent_taxi.taxiservice.domain.Taxi;
 import intelligent_taxi.taxiservice.dto.TaxiResponse;
+import intelligent_taxi.taxiservice.utility.CommonUtils;
 
 public class TaxiMapper {
     public static TaxiResponse entityToDto(Taxi taxi) {
+        if (CommonUtils.isNull(taxi)) {
+            return new TaxiResponse();
+        }
+
         return TaxiResponse.builder()
                 .id(taxi.getId())
                 .taxiGrade(taxi.getTaxiGrade())
