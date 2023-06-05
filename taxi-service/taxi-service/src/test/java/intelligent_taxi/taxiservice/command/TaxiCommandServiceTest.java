201c09f79e58f6411f47df511520cd74d3581265
liveforone <yc4852@gmail.com>
2023-06-05 18:02:16 +0900

    Add taxi command service test.

---
+++ taxi-service/taxi-service/src/test/java/intelligent_taxi/taxiservice/command/TaxiCommandServiceTest.java
@@ -0,0 +1,34 @@
+package intelligent_taxi.taxiservice.command;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class TaxiCommandServiceTest {
+
+    @Test
+    @Transactional
+    void createTaxiTest() {
+    }
+
+    @Test
+    @Transactional
+    void updateRegionTest() {
+    }
+
+    @Test
+    @Transactional
+    void updateGradeTest() {
+    }
+
+    @Test
+    @Transactional
+    void deleteOneByUsernameTest() {
+    }
+
+    @Test
+    @Transactional
+    void deleteOneByIdTest() {
+    }
+}
