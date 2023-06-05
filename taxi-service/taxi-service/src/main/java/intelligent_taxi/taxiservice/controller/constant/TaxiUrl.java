ccd544a022d2bd92d8bedaa5625d132ccb06a91a
liveforone <yc4852@gmail.com>
2023-06-05 17:49:29 +0900

    Add taxi my page.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/constant/TaxiUrl.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/constant/TaxiUrl.java
@@ -2,6 +2,10 @@ package intelligent_taxi.taxiservice.controller.constant;
 
 public final class TaxiUrl {
 
+    //==query==//
+    public static final String MY_PAGE = "/taxi/my-page";
+
+    //==command==//
     public static final String CREATE = "/create/taxi";
     public static final String UPDATE_REGION = "/update/region/{id}";
     public static final String UPDATE_GRADE = "/update/grade/{id}";
