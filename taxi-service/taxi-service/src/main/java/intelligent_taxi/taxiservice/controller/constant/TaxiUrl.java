c681d886a19cf0880cb0b8f1207aa3c0dba3bccd
liveforone <yc4852@gmail.com>
2023-06-05 17:34:10 +0900

    Add delete taxi url.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/constant/TaxiUrl.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/constant/TaxiUrl.java
@@ -5,4 +5,5 @@ public final class TaxiUrl {
     public static final String CREATE = "/create/taxi";
     public static final String UPDATE_REGION = "/update/region/{id}";
     public static final String UPDATE_GRADE = "/update/grade/{id}";
+    public static final String DELETE_TAXI = "/delete/taxi/{id}";
 }
