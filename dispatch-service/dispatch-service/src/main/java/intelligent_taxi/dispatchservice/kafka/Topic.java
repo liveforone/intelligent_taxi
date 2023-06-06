17994f26eb72929cf56aa838af432224bf562090
liveforone <yc4852@gmail.com>
2023-06-06 16:26:55 +0900

    Add topic.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/kafka/Topic.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/kafka/Topic.java
@@ -4,4 +4,9 @@ public final class Topic {
 
     private Topic() {}
 
+    //==producer==//
+    public static final String REQUEST_ORDER = "request-order";
+    public static final String REQUEST_CALCULATE = "request-calculate";
+
+    //==consumer==//
 }
