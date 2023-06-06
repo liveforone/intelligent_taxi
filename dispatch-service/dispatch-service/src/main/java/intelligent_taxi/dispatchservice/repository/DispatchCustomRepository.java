33e72a90f409f524e14bd8ddc55a3795183c92bd
liveforone <yc4852@gmail.com>
2023-06-06 19:39:10 +0900

    Add find one by id or username query method.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/repository/DispatchCustomRepository.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/repository/DispatchCustomRepository.java
@@ -1,4 +1,8 @@
 package intelligent_taxi.dispatchservice.repository;
 
+import intelligent_taxi.dispatchservice.domain.Dispatch;
+
 public interface DispatchCustomRepository {
+    Dispatch findOneByUsername(String username);
+    Dispatch findOneById(Long id);
 }
--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/repository/DispatchRepositoryImpl.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/repository/DispatchRepositoryImpl.java
@@ -1,6 +1,8 @@
 package intelligent_taxi.dispatchservice.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import intelligent_taxi.dispatchservice.domain.Dispatch;
+import intelligent_taxi.dispatchservice.domain.QDispatch;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,19 @@ import org.springframework.stereotype.Repository;
 public class DispatchRepositoryImpl implements DispatchCustomRepository {
 
     private final JPAQueryFactory queryFactory;
+    QDispatch dispatch = QDispatch.dispatch;
+
+    public Dispatch findOneByUsername(String username) {
+        return queryFactory
+                .selectFrom(dispatch)
+                .where(dispatch.username.eq(username))
+                .fetchOne();
+    }
+
+    public Dispatch findOneById(Long id) {
+        return queryFactory
+                .selectFrom(dispatch)
+                .where(dispatch.id.eq(id))
+                .fetchOne();
+    }
 }
