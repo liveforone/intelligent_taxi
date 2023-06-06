c55bb651a6c59c77662eb55d8928e96441b06eea
liveforone <yc4852@gmail.com>
2023-06-06 16:58:33 +0900

    Add dispatch entity.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/domain/Dispatch.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/domain/Dispatch.java
@@ -1,15 +1,46 @@
 package intelligent_taxi.dispatchservice.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EntityListeners;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(AuditingEntityListener.class)
 public class Dispatch {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Double presentLatitude;
+
+    @Column(nullable = false)
+    private Double presentLongitude;
+
+    @Column(nullable = false)
+    private Double destinationLatitude;
+
+    @Column(nullable = false)
+    private Double destinationLongitude;
+
+    @Column(nullable = false)
+    private String username;
+
+    private long price;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
 }
