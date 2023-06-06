d716d1cdb7e913f25aa19d03aa8b268a1b637ec9
liveforone <yc4852@gmail.com>
2023-06-06 20:04:18 +0900

    Add auth is not member message.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/restResponse/ResponseMessage.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/restResponse/ResponseMessage.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ResponseMessage {
 
+    AUTH_IS_NOT_MEMBER(401, "일반회원이 아닙니다."),
     DISPATCH_SAVE_SUCCESS(201, "배차를 성공적으로 등록하였습니다.");
 
     private final int status;
