60dd2baf2f7a8e21e53d8f3c2c08d019d862b954
liveforone <yc4852@gmail.com>
2023-06-06 12:03:03 +0900

    Initial dispatch-service.

---
+++ dispatch-service/dispatch-service/src/test/java/intelligent_taxi/dispatchservice/DispatchServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package intelligent_taxi.dispatchservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DispatchServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
