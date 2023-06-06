00d572eb026d747b039e590ee457f23b9b69b92e
liveforone <yc4852@gmail.com>
2023-06-06 20:04:43 +0900

    Add validate auth is member validator.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/validator/ControllerValidator.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/validator/ControllerValidator.java
@@ -1,8 +1,15 @@
 package intelligent_taxi.dispatchservice.validator;
 
+import intelligent_taxi.dispatchservice.authentication.Role;
+import intelligent_taxi.dispatchservice.controller.restResponse.ResponseMessage;
+import intelligent_taxi.dispatchservice.exception.DispatchCustomException;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ControllerValidator {
-
+    public void validateAuthIsMember(String auth) {
+        if (!auth.equals(Role.MEMBER.getAuth())) {
+            throw new DispatchCustomException(ResponseMessage.AUTH_IS_NOT_MEMBER);
+        }
+    }
 }
