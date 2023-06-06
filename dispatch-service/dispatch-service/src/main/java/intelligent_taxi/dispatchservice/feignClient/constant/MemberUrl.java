8cc50b8faad6e7b7c9c8c4662dc46152ac57f710
liveforone <yc4852@gmail.com>
2023-06-06 16:36:46 +0900

    Add member url.

---
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/feignClient/constant/MemberUrl.java
@@ -0,0 +1,6 @@
+package intelligent_taxi.dispatchservice.feignClient.constant;
+
+public final class MemberUrl {
+    public static final String BASE = "taxi-user-service";
+    public static final String MY_BANKBOOK_NUM = "/provide/my-bankbookNum/{username}";
+}
