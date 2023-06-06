79b822abbd6f0878f8b8331bd9e4333926d9942b
liveforone <yc4852@gmail.com>
2023-06-06 18:35:58 +0900

    Add dispatch response.

---
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/dto/dispatch/DispatchResponse.java
@@ -0,0 +1,25 @@
+package intelligent_taxi.dispatchservice.dto.dispatch;
+
+import intelligent_taxi.dispatchservice.domain.DispatchState;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DispatchResponse {
+
+    private Long id;
+    private Double presentLatitude;
+    private Double presentLongitude;
+    private Double destinationLatitude;
+    private Double destinationLongitude;
+    private long price;
+    private DispatchState dispatchState;
+    private LocalDateTime createdDate;
+}
