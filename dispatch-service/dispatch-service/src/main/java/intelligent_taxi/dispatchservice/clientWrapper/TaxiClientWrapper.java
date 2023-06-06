ab1a76593050f737b1b71bc62a4ec532bb4f4ca3
liveforone <yc4852@gmail.com>
2023-06-06 12:31:32 +0900

    Add Taxi client wrapper.

---
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/clientWrapper/TaxiClientWrapper.java
@@ -0,0 +1,24 @@
+package intelligent_taxi.dispatchservice.clientWrapper;
+
+import intelligent_taxi.dispatchservice.dto.taxi.TaxiResponse;
+import intelligent_taxi.dispatchservice.feignClient.TaxiClient;
+import intelligent_taxi.dispatchservice.feignClient.constant.CircuitLog;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class TaxiClientWrapper {
+    private final TaxiClient taxiClient;
+    private final CircuitBreakerFactory<?, ?> circuitBreakerFactory;
+
+    public TaxiResponse getTaxiInfo(Long taxiId) {
+        return circuitBreakerFactory
+                .create(CircuitLog.DISPATCH_CIRCUIT_LOG.getValue())
+                .run(
+                        () -> taxiClient.getTaxiInfo(taxiId),
+                        throwable -> new TaxiResponse()
+                );
+    }
+}
