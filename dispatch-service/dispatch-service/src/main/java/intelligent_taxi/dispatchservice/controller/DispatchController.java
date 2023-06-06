c215ba5b1e46ed6423d8aad125d38a3c9be5b60a
liveforone <yc4852@gmail.com>
2023-06-06 20:02:08 +0900

    Refactor dispatch member home url.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/DispatchController.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/DispatchController.java
@@ -2,10 +2,15 @@ package intelligent_taxi.dispatchservice.controller;
 
 import intelligent_taxi.dispatchservice.authentication.AuthenticationInfo;
 import intelligent_taxi.dispatchservice.query.DispatchQueryService;
+import intelligent_taxi.dispatchservice.validator.ControllerValidator;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static intelligent_taxi.dispatchservice.controller.constant.DispatchUrl.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -14,6 +19,12 @@ public class DispatchController {
 
     private final DispatchQueryService dispatchQueryService;
     private final AuthenticationInfo authenticationInfo;
+    private final ControllerValidator controllerValidator;
 
-    @GetMapping
+    @GetMapping(DISPATCH_MEMBER_HOME)
+    public ResponseEntity<?> dispatchMemberHome(
+            HttpServletRequest request
+    ) {
+
+    }
 }
--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/constant/DispatchUrl.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/constant/DispatchUrl.java
@@ -1,5 +1,5 @@
 package intelligent_taxi.dispatchservice.controller.constant;
 
 public final class DispatchUrl {
-    public static final String DISPATCH_HOME = "/dispatch/home";
+    public static final String DISPATCH_MEMBER_HOME = "/dispatch/member/home";
 }
