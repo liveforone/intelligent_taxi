1fe16c92d57698bfd4d87169064a2bde0c45178c
liveforone <yc4852@gmail.com>
2023-06-06 16:42:50 +0900

    Add check balance feign method.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/feignClient/PayClient.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/feignClient/PayClient.java
@@ -1,7 +1,15 @@
 package intelligent_taxi.dispatchservice.feignClient;
 
+import intelligent_taxi.dispatchservice.dto.pay.CheckBalanceRequest;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = )
+import static intelligent_taxi.dispatchservice.feignClient.constant.PayUrl.*;
+
+@FeignClient(name = BASE)
 public interface PayClient {
+
+    @PostMapping(CHECK_BALANCE)
+    boolean checkBalance(@RequestBody CheckBalanceRequest requestDto);
 }
