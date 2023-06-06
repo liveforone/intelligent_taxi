83e15deb8e1355d406c1d3131ae148f1f1b2f61a
liveforone <yc4852@gmail.com>
2023-06-06 16:46:10 +0900

    Add pay client wrapper.

---
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/clientWrapper/PayClientWrapper.java
@@ -0,0 +1,25 @@
+package intelligent_taxi.dispatchservice.clientWrapper;
+
+import intelligent_taxi.dispatchservice.dto.pay.CheckBalanceRequest;
+import intelligent_taxi.dispatchservice.feignClient.PayClient;
+import intelligent_taxi.dispatchservice.feignClient.constant.CircuitLog;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PayClientWrapper {
+
+    private final PayClient payClient;
+    private final CircuitBreakerFactory<?, ?> circuitBreakerFactory;
+
+    public boolean checkBalance(CheckBalanceRequest requestDto) {
+        return circuitBreakerFactory
+                .create(CircuitLog.DISPATCH_CIRCUIT_LOG.getValue())
+                .run(
+                        () -> payClient.checkBalance(requestDto),
+                        throwable -> false
+                );
+    }
+}
