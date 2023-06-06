3d6639cc0c24aa5848c3a67974d357d5e051a778
liveforone <yc4852@gmail.com>
2023-06-06 16:34:29 +0900

    Add pay url and check balance url.

---
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/feignClient/constant/PayUrl.java
@@ -0,0 +1,6 @@
+package intelligent_taxi.dispatchservice.feignClient.constant;
+
+public final class PayUrl {
+    public static final String BASE = "pay-service";
+    public static final String CHECK_BALANCE = "/provide/check/balance/{bankbookNum}";
+}
