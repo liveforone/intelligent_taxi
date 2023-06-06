6f5eb19866a5f0071f1c550d5b7a8958d322baa7
liveforone <yc4852@gmail.com>
2023-06-06 16:31:16 +0900

    Add kafka log.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/kafka/KafkaLog.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/kafka/KafkaLog.java
@@ -9,7 +9,9 @@ public enum KafkaLog {
 
     KAFKA_RECEIVE_LOG("Consumer receive Kafka Message -> "),
     KAFKA_SEND_LOG("Kafka send Success | Topic : "),
-    KAFKA_NULL_LOG("!! Kafka Message is Null !!");
+    KAFKA_NULL_LOG("!! Kafka Message is Null !!"),
+    ORDER_FAIL("Order Fail | Dispatch Id : "),
+    CALCULATE_FAIL("Calculate Fail | Dispatch Id : ");
 
     private final String value;
 }
