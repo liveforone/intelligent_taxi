8c29c4ac327201a26b2e37b30c67f973cfe650df
liveforone <yc4852@gmail.com>
2023-06-06 19:53:06 +0900

    Add get dispatch by id service method.

---
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/query/DispatchQueryService.java
@@ -0,0 +1,22 @@
+package intelligent_taxi.dispatchservice.query;
+
+import intelligent_taxi.dispatchservice.dto.dispatch.DispatchResponse;
+import intelligent_taxi.dispatchservice.query.util.DispatchMapper;
+import intelligent_taxi.dispatchservice.repository.DispatchRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class DispatchQueryService {
+
+    private final DispatchRepository dispatchRepository;
+
+
+
+    public DispatchResponse getDispatchById(Long id) {
+        return DispatchMapper.entityToDto(dispatchRepository.findOneById(id));
+    }
+}
