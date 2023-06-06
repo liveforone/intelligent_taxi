2063f2826262840952d4d4782ad8f755b09a9746
liveforone <yc4852@gmail.com>
2023-06-06 18:27:05 +0900

    Add create domain method.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/domain/Dispatch.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/domain/Dispatch.java
@@ -1,5 +1,7 @@
 package intelligent_taxi.dispatchservice.domain;
 
+import intelligent_taxi.dispatchservice.domain.policy.PriceCalculator;
+import intelligent_taxi.dispatchservice.dto.dispatch.DispatchRequest;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -43,4 +45,24 @@ public class Dispatch {
 
     @LastModifiedDate
     private LocalDateTime modifiedDate;
+
+    private Dispatch(Double presentLatitude, Double presentLongitude, Double destinationLatitude, Double destinationLongitude, String username, long price) {
+        this.presentLatitude = presentLatitude;
+        this.presentLongitude = presentLongitude;
+        this.destinationLatitude = destinationLatitude;
+        this.destinationLongitude = destinationLongitude;
+        this.username = username;
+        this.price = price;
+    }
+
+    public static Dispatch create(DispatchRequest requestDto, String username) {
+        return new Dispatch(
+                requestDto.getPresentLatitude(),
+                requestDto.getPresentLongitude(),
+                requestDto.getDestinationLatitude(),
+                requestDto.getDestinationLongitude(),
+                username,
+                PriceCalculator.calculatePrice(requestDto)
+        );
+    }
 }
