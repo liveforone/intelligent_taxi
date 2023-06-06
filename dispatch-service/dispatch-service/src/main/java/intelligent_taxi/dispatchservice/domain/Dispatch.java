f6cb2e5d87f0501d7d51292e400705998284d4b7
liveforone <yc4852@gmail.com>
2023-06-06 16:48:12 +0900

    Add jpa auditing entity.

---
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/domain/Dispatch.java
@@ -0,0 +1,15 @@
+package intelligent_taxi.dispatchservice.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Dispatch {
+}
