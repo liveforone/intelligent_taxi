52e641474604154d4fa6938ed41e13ab3e977560
liveforone <yc4852@gmail.com>
2023-06-07 18:47:51 +0900

    Add dispatch is already finish.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/restResponse/ResponseMessage.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/restResponse/ResponseMessage.java
@@ -15,7 +15,8 @@ public enum ResponseMessage {
     AUTH_IS_NOT_MEMBER(401, "일반회원이 아닙니다."),
     AUTH_IS_NOT_TAXI(401, "택시회원이 아닙니다."),
     DISPATCH_IS_NULL(404, "배차가 존재하지 않습니다."),
-    NOT_MATCH_USERNAME(400, "일치하지 않는 회원입니다.");
+    NOT_MATCH_USERNAME(400, "일치하지 않는 회원입니다."),
+    DISPATCH_IS_ALREADY_FINISH(400, "이미 배차가 완료되었습니다.\n취소가 불가능합니다.");
 
     private final int status;
     private final String value;
