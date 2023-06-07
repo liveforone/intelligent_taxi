f1efe89f5a734b94f24b616954c62544067c1018
liveforone <yc4852@gmail.com>
2023-06-07 19:00:58 +0900

    Refactor rename rollabck.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/kafka/KafkaLog.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/kafka/KafkaLog.java
@@ -10,8 +10,8 @@ public enum KafkaLog {
     KAFKA_RECEIVE_LOG("Consumer receive Kafka Message -> "),
     KAFKA_SEND_LOG("Kafka send Success | Topic : "),
     KAFKA_NULL_LOG("!! Kafka Message is Null !!"),
-    ORDER_FAIL("Order Fail | Dispatch Id : "),
-    CALCULATE_FAIL("Calculate Fail | Dispatch Id : ");
+    ORDER_FAIL_ROLLBACK_DISPATCH("Order Fail | Dispatch Id : "),
+    CALCULATE_FAIL_ROLLBACK_DISPATCH("Calculate Fail | Dispatch Id : ");
 
     private final String value;
 }
