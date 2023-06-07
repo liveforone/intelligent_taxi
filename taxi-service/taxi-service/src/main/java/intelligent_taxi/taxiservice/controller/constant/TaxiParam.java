a9db2b2ace4d23d7bb5cc12bcc536ab2a74a6895
liveforone <yc4852@gmail.com>
2023-06-07 20:10:44 +0900

    Add usernaem param.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/constant/TaxiParam.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/controller/constant/TaxiParam.java
@@ -2,4 +2,5 @@ package intelligent_taxi.taxiservice.controller.constant;
 
 public final class TaxiParam {
     public static final String ID = "id";
+    public static final String USERNAME = "username";
 }
