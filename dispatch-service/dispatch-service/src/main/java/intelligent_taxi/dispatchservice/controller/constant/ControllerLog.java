4c0bcbb1e88309c75d4d7851a8892c90e19ac27f
liveforone <yc4852@gmail.com>
2023-06-07 22:00:43 +0900

    Refactor dispatch succes.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/constant/ControllerLog.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/constant/ControllerLog.java
@@ -9,7 +9,7 @@ public enum ControllerLog {
 
     CREATE_DISPATCH_SUCCESS("Create Dispatch Success"),
     REMOVE_DISPATCH_SUCCESS("Remove Dispatch Success | ID : "),
-    REQUEST_DISPATCH_SUCCESS("Request Dispatch Success | ID : ");
+    REQUEST_DISPATCH_SUCCESS("Request Dispatch Success");
 
     private final String log;
 }
