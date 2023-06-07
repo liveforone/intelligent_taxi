81e1af3df22cd80c5c2761daaf4400e474fd03a9
liveforone <yc4852@gmail.com>
2023-06-07 18:41:56 +0900

    Add remove dispatch method.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/command/DispatchCommandService.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/command/DispatchCommandService.java
@@ -3,6 +3,7 @@ package intelligent_taxi.dispatchservice.command;
 import intelligent_taxi.dispatchservice.domain.Dispatch;
 import intelligent_taxi.dispatchservice.dto.dispatch.DispatchRequest;
 import intelligent_taxi.dispatchservice.repository.DispatchRepository;
+import intelligent_taxi.dispatchservice.validator.ServiceValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,9 +14,15 @@ import org.springframework.transaction.annotation.Transactional;
 public class DispatchCommandService {
 
     private final DispatchRepository dispatchRepository;
+    private final ServiceValidator serviceValidator;
 
     public Long createDispatch(DispatchRequest requestDto, String username) {
         Dispatch dispatch = Dispatch.create(requestDto, username);
         return dispatchRepository.save(dispatch).getId();
+    }
+
+    public void removeDispatch(Long id, String username) {
+        Dispatch dispatch = serviceValidator.validateUsername(id, username);
+        dispatchRepository.delete(dispatch);
     }
 }
