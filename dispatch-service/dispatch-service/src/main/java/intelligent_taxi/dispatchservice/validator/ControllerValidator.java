245606d457de16c7807387448ae4fabb9644249f
liveforone <yc4852@gmail.com>
2023-06-07 18:25:43 +0900

    Add validate binding result.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/validator/ControllerValidator.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/validator/ControllerValidator.java
@@ -2,11 +2,24 @@ package intelligent_taxi.dispatchservice.validator;
 
 import intelligent_taxi.dispatchservice.authentication.Role;
 import intelligent_taxi.dispatchservice.controller.restResponse.ResponseMessage;
+import intelligent_taxi.dispatchservice.exception.BindingCustomException;
 import intelligent_taxi.dispatchservice.exception.DispatchCustomException;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+
+import java.util.Objects;
 
 @Component
 public class ControllerValidator {
+
+    public void validateBinding(BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorMessage = Objects
+                    .requireNonNull(bindingResult.getFieldError())
+                    .getDefaultMessage();
+            throw new BindingCustomException(errorMessage);
+        }
+    }
 
     public void validateAuthIsMember(String auth) {
         if (!auth.equals(Role.MEMBER.getAuth())) {
