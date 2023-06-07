1b09b2a1ab18b6adb5348a81cc67327287a76b05
liveforone <yc4852@gmail.com>
2023-06-07 19:04:03 +0900

    Add request dispatch url.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/constant/DispatchUrl.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/constant/DispatchUrl.java
@@ -4,4 +4,5 @@ public final class DispatchUrl {
     public static final String DISPATCH_MEMBER_HOME = "/dispatch/member/home";
     public static final String CREATE_DISPATCH = "/dispatch/create";
     public static final String REMOVE_DISPATCH = "/dispatch/remove/{id}";
+    public static final String REQUEST_DISPATCH = "/dispatch/request";
 }
