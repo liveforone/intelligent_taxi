a7a368dbfd6a885a83cb55d1bdd75b809a6ddeb2
liveforone <yc4852@gmail.com>
2023-06-07 18:57:11 +0900

    Add producer rollback topic.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/kafka/Topic.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/kafka/Topic.java
@@ -4,9 +4,13 @@ public final class Topic {
 
     private Topic() {}
 
-    //==producer==//
+    //==producer-request==//
     public static final String REQUEST_ORDER = "request-order";
     public static final String REQUEST_CALCULATE = "request-calculate";
+
+    //==producer-rollback==//
+    public static final String ORDER_FAIL_ROLLBACK_CALCULATE = "order-fail-rollback-calculate";
+    public static final String CALCULATE_FAIL_ROLLBACK_ORDER = "calculate-fail-rollback-order";
 
     //==consumer==//
     public static final String ORDER_FAIL_ROLLBACK_DISPATCH = "order-fail-rollback-dispatch";
