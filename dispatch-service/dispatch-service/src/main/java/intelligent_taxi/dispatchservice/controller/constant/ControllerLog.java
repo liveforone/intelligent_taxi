cfd03afc46535fa5feb3e0089d73a2c5b136184c
liveforone <yc4852@gmail.com>
2023-06-07 18:30:13 +0900

    Add create dispatch success log.

---
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/constant/ControllerLog.java
@@ -0,0 +1,13 @@
+package intelligent_taxi.dispatchservice.controller.constant;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ControllerLog {
+
+    CREATE_DISPATCH_SUCCESS("Create Dispatch Success");
+
+    private final String log;
+}
