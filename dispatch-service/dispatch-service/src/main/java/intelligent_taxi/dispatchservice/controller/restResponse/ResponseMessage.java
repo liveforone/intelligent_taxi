42cbb54fcd2d4fe29c031c201be127375fb26810
liveforone <yc4852@gmail.com>
2023-06-07 22:03:37 +0900

    Add dispatch success message.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/restResponse/ResponseMessage.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/restResponse/ResponseMessage.java
@@ -10,6 +10,7 @@ public enum ResponseMessage {
     //==success==//
     DISPATCH_SAVE_SUCCESS(201, "배차를 성공적으로 등록하였습니다."),
     REMOVE_DISPATCH_SUCCESS(200, "배차를 성공적으로 등록 취소 하였습니다."),
+    DISPATCH_SUCCESS(201, "배차를 성공하였습니다."),
 
     //==fail==//
     AUTH_IS_NOT_MEMBER(401, "일반회원이 아닙니다."),
