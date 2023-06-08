fdcf18264ed3fc173a5624705bf4006079c38149
liveforone <yc4852@gmail.com>
2023-06-08 20:13:42 +0900

    Add duplicate entity valude.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/exception/DispatchControllerAdvice.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/exception/DispatchControllerAdvice.java
@@ -1,5 +1,6 @@
 package intelligent_taxi.dispatchservice.exception;
 
+import intelligent_taxi.dispatchservice.controller.restResponse.ResponseMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -7,6 +8,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class DispatchControllerAdvice {
+
+    public static ResponseEntity<?> duplicateEntityValue() {
+        return ResponseEntity
+                .status(ResponseMessage.DUPLICATE_ENTITY_VALUE.getStatus())
+                .body(ResponseMessage.DUPLICATE_ENTITY_VALUE.getValue());
+    }
 
     @ExceptionHandler(DispatchCustomException.class)
     protected ResponseEntity<?> dispatchCustomHandle(DispatchCustomException customException) {
