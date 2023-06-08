8bcc060850260b9b1bb7ca02faa0292056dbee2b
liveforone <yc4852@gmail.com>
2023-06-08 20:13:03 +0900

    Add duplicate entity value.

--- dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/restResponse/ResponseMessage.java
+++ dispatch-service/dispatch-service/src/main/java/intelligent_taxi/dispatchservice/controller/restResponse/ResponseMessage.java
@@ -13,10 +13,11 @@ public enum ResponseMessage {
     DISPATCH_SUCCESS(201, "배차를 성공하였습니다."),
 
     //==fail==//
+    DISPATCH_IS_NULL(404, "배차가 존재하지 않습니다."),
+    DUPLICATE_ENTITY_VALUE(400, "중복되는 데이터가 존재합니다."),
     CHECK_BALANCE_FAIL(400, "결제 승인에 실패하였습니다."),
     AUTH_IS_NOT_MEMBER(401, "일반회원이 아닙니다."),
     AUTH_IS_NOT_TAXI(401, "택시회원이 아닙니다."),
-    DISPATCH_IS_NULL(404, "배차가 존재하지 않습니다."),
     NOT_MATCH_USERNAME(400, "일치하지 않는 회원입니다."),
     DISPATCH_IS_ALREADY_FINISH(400, "이미 배차가 완료되었습니다.\n취소가 불가능합니다.");
 
