697de910636fab29a84d9a9e1e51386c99f2f87a
liveforone <yc4852@gmail.com>
2023-06-08 19:55:54 +0900

    Add null check lambda.

--- taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/query/TaxiQueryService.java
+++ taxi-service/taxi-service/src/main/java/intelligent_taxi/taxiservice/query/TaxiQueryService.java
@@ -1,6 +1,8 @@
 package intelligent_taxi.taxiservice.query;
 
+import intelligent_taxi.taxiservice.controller.restResponse.ResponseMessage;
 import intelligent_taxi.taxiservice.dto.TaxiResponse;
+import intelligent_taxi.taxiservice.exception.TaxiCustomException;
 import intelligent_taxi.taxiservice.query.util.TaxiMapper;
 import intelligent_taxi.taxiservice.repository.TaxiRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +17,12 @@ public class TaxiQueryService {
     private final TaxiRepository taxiRepository;
 
     public TaxiResponse getTaxiByUsername(String username) {
-        return TaxiMapper.entityToDto(taxiRepository.findOneByUsername(username));
+        return TaxiMapper.entityToDto(taxiRepository.findOneByUsername(username)
+                .orElseThrow(() -> new TaxiCustomException(ResponseMessage.TAXI_IS_NULL)));
     }
 
     public TaxiResponse getTaxiById(Long id) {
-        return TaxiMapper.entityToDto(taxiRepository.findOneById(id));
+        return TaxiMapper.entityToDto(taxiRepository.findOneById(id)
+                .orElseThrow(() -> new TaxiCustomException(ResponseMessage.TAXI_IS_NULL)));
     }
 }
